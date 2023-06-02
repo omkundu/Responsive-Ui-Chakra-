9e4e9a064d57daa44e084b92a0cede4e3d0a5655
omkundu <omkundu156@gmail.com>
2023-06-02 12:01:44 +0530

    third-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,40 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, Flex } from "@chakra-ui/react";
+import React from "react";
+import { Link, Link as ReactRouterLink } from "react-router-dom";
+const links = [
+  {
+    to: "/",
+    label: "Home",
+  },
+  {
+    to: "/about",
+    label: "About",
+  },
+  {
+    to: "/conatct",
+    label: "Contact",
+  },
+  {
+    to: "/login",
+    label: "Login",
+  },
+];
 
 const Navbar = () => {
   return (
-   <Box>Navbar</Box>
-  )
-}
+    <Box>
+      <Box pos="fixed" top="1rem" right="1rem" align="center">
+        <Flex display={{ base: "none", md: "flex" }}>
+          {links.map((link) => (
+            <Link as={ReactRouterLink} to={link.to}>
+              <Button variant="ghost">{link.label}</Button>
 
-export default Navbar
+            </Link>
+          ))}
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default Navbar;
