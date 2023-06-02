878dc2b47a3a5db21b751f2d577a2d4661064bbb
omkundu <omkundu156@gmail.com>
2023-06-02 13:14:46 +0530

    fifth-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { Box, Button, Flex, IconButton } from "@chakra-ui/react";
 import React from "react";
 import { Link, Link as ReactRouterLink } from "react-router-dom";
-import {HamburgerIcon} from "@chakra-ui/icons"
+import { HamburgerIcon } from "@chakra-ui/icons";
+import {CloseIcon} from "@chakra-ui/icons"
+import { useBoolean } from '@chakra-ui/react'
 const links = [
   {
     to: "/",
@@ -22,19 +24,50 @@ const links = [
 ];
 
 const Navbar = () => {
+    const [flag,setFlag]=useBoolean(false)
   return (
     <Box>
       <Box pos="fixed" top="1rem" right="1rem" align="center">
         <Flex display={{ base: "none", md: "flex" }}>
           {links.map((link) => (
-            <Link as={ReactRouterLink} to={link.to}>
+            <Link key={link.to} as={ReactRouterLink} to={link.to}>
               <Button variant="ghost">{link.label}</Button>
-
             </Link>
           ))}
         </Flex>
-        <IconButton icon={<HamburgerIcon/>}></IconButton>
+        <IconButton
+        onClick={setFlag.on}
+          display={{ base: "block", md: "none" }}
+          icon={<HamburgerIcon />}
+        ></IconButton>
       </Box>
+     {flag && ( <Box
+        pos="fixed"
+        top="0"
+        left="0"
+        w="100vw"
+        h="100vh"
+        bgColor="white"
+        zIndex={20}
+        overflow="auto"
+        display={{ base: "block", md: "none" }}
+      >
+        <Flex>
+            <Box pos="fixed" top="1rem" right="1rem">
+                <IconButton  icon={<CloseIcon/>}   onClick={setFlag.off}
+></IconButton>
+            </Box>
+        </Flex>
+        
+          <Flex direction="column" align="center" marginTop="3rem">
+            {links.map((link) => (
+              <Link key={link.to} as={ReactRouterLink} to={link.to}>
+                <Button variant="ghost">{link.label}</Button>
+              </Link>
+            ))}
+          </Flex>
+        
+      </Box>)}
     </Box>
   );
 };
