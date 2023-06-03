e29f5bbc115dc3a6fb0076fd6659652b9cc03e1b
omkundu <omkundu156@gmail.com>
2023-06-03 11:37:06 +0530

    second-Day-First-Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import AllRoutes from './Components/AllRoutes';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Box marginTop="3rem">
       <AllRoutes/>
+      </Box>
       <Footer/>
     </div>
   );
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,26 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Button, Flex, HStack, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const dummyLinks = new Array(12).fill(0).map((_, i) => `Link ${i+1}`);
   return (
-   <Box>Footer</Box>
-  )
-}
+    <VStack py={10} bg="gray.200" mt={4}>
+      <Flex align="center" justify="center">
+        <SimpleGrid columns={{ base: 2, md: 3, lg: 4 }} spacing="4rem">
+          {dummyLinks.map((el) => (
+            <Link>
+            <Button variant="ghost">{el}</Button></Link>
+          ))}
+        </SimpleGrid>
+      </Flex>
+      <HStack>
+        <Text>&copy; {new Date().getFullYear()} Om Coffee Shop</Text>
+        <Text>|</Text>
+        <Text>All right reserved</Text>
+      </HStack>
+    </VStack>
+  );
+};
 
-export default Footer
+export default Footer;
